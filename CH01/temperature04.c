be51e0f07b34b129d1967b647dcb5364e0c58100
Steven Z <stevencsu@163.com>
2019-03-22 12:30:33 +0800

    add 1.3 1.4 and .gitignore

---
+++ CH01/temperature04.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+#define LOWER 0
+#define UPPER 300
+#define STEP 30
+
+int main()
+{
+    for(int fahr = UPPER; fahr >= LOWER; fahr-=STEP)
+    {
+        /* code */
+        printf("%3d %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
+    }
+    
+}
