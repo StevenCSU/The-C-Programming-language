53f4c7266f2c5cb4862e3259cc00ecb570d65354
Steven Z <stevencsu@163.com>
2019-03-22 11:58:50 +0800

    add 1.1 and 1.2

---
+++ CH01/temperature.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/* 当fahr= 0， 20， ..., 300时，分别打印华氏温度和摄氏温度对照表 */
+int main()
+{
+    int fahr, celsius;
+    int lower, upper, step;
+
+    lower = 0;
+    upper = 300;
+    step = 20;
+
+    fahr = lower;
+    while(fahr <= upper) {
+        celsius = 5 * (fahr - 32) / 9;
+        printf("%3d %6d\n", fahr, celsius);
+        fahr = fahr + step;
+    }
+    
+}
